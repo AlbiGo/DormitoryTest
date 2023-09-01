67ded2f98b1c93a3ac9fef49aea33cf0078135f6
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2023-09-01 20:15:23 +0200

    Added migrations

---
+++ DataLayer/DBContext/DormitoryDBContext.cs
@@ -0,0 +1,34 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DBContext
+{
+    public class DormitoryDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DormitoryDBContext()
+        {
+        }
+        public DormitoryDBContext(DbContextOptions<DormitoryDBContext> options)
+          : base(options)
+        {   
+        }
+     
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=WINDOWS-4PGG12B; Initial Catalog=Dormitory;Integrated Security=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity => {
+                entity.HasKey(k => k.Id);
+            });
+        }
+    }
+}
+
