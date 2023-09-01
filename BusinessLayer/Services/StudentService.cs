aeec1daad0a4be24d9b90c9deee2bd397f2122f4
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2023-09-01 20:32:50 +0200

    Add 1 service

---
+++ BusinessLayer/Services/StudentService.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Interfaces;
+using DataLayer.DBContext;
+using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly DormitoryDBContext _dormitoryDBContext;
+
+        public StudentService()
+        {
+            _dormitoryDBContext = new DormitoryDBContext();
+        }
+
+        public async Task<List<Student>> GetAllStudents()
+        {
+            var students =  await _dormitoryDBContext.Students.ToListAsync();
+            return students;
+        }
+    }
+}
--- Dormitory/Program.cs
+++ Dormitory/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BusinessLayer.Interfaces;
+using BusinessLayer.Services;
+
 Console.WriteLine("Hello, World!");
+
+
+IStudentService studentService = new StudentService();
+
+var allStudents = await studentService.GetAllStudents();
+
+allStudents.ForEach(student =>
+{
+    Console.WriteLine(student.Name);
+});
